546278e9b60bd3dd8456eea1d38f99624e9f861c
Jmisel33 <jmiselnicky@sjnma.org>
2017-03-06 19:56:23 -0600

    completed the game

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from random import randint 
 
+print("This is a dice rolling game. Each player will roll and who ever gets this highest score out of 3 games wins")
+
+winNumber = 3
+
 playerOne = raw_input("what is your name player one? ")
 playerTwo = raw_input("What is your name player two? ")
 
+playerOneWin = 0
+playerTwoWin = 0
 
+Roll = raw_input("press enter to roll")
+rollOne = randint(1, 6)
+rollTwo = randint(1, 6)
+
+while(playerOne != winNumber and playerTwo != winNumber):
+	playerOne = randint(1, 6)
+	print(playerOne)
+	playerTwo = randint(1, 6)
+	print(playerTwo )
+	if(playerOne > playerTwo ):
+		print('player one won that round')
+		playerOneWin += 1
+	if(playerTwo  > playerOne ):
+		print("player two won that round")
+		playerTwoWin += 1
+if(playerOneWin == winNumber):
+	print('Congrates player one won the game')
+if(playerTwoWin == winNumber):
+	print('Congrats player two won the game')
